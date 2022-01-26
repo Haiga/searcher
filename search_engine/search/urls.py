5287ed7c92d7e099e0242f41eea525d02ce7901a
Pedro <pedro_rga@hotmail.com>
2022-01-25 22:41:22 -0300

    update mudando a url

--- search_engine/search/urls.py
+++ search_engine/search/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 app_name = 'search'
 urlpatterns = [
+    url(r'^search_engine/', include([
     path('', views.index, name='index'),
     path('search', views.search, name='search'),
     path('search_comparison', views.search_comparison, name='search_comparison'),
@@ -13,4 +15,5 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('erro', views.erro, name='erro'),
+    ])),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
