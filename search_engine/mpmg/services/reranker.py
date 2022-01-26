173b1cb815702df2cb728abbf9280165226e0d97
Pedro <pedro_rga@hotmail.com>
2022-01-25 21:49:58 -0300

    update mudando a url

--- search_engine/mpmg/services/reranker.py
+++ search_engine/mpmg/services/reranker.py
@@ -10,9 +10,8 @@ class Reranker():
         self.model = self.get_sentence_model()
     
     # def get_sentence_model(self, model_path="prajjwal1/bert-tiny"):
-    def get_sentence_model(self, model_path="/home/silvapedro/searcher/search_engine/bert-base-portuguese-cased/"):
-        args = {'from_tf':True}
-        word_embedding_model = models.Transformer(model_path, max_seq_length=500, model_args=args)
+    def get_sentence_model(self, model_path="neuralmind/bert-base-portuguese-cased"):
+        word_embedding_model = models.Transformer(model_path, max_seq_length=500)
         pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
 
         return SentenceTransformer(modules=[word_embedding_model, pooling_model])
